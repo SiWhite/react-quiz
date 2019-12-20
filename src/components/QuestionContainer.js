162f7845931f64caf6b8fe7f5f98171029a8795e
Simon <simon@capiche.co.nz>
2019-12-21 12:38:26 +1300

    splice correct answer into incorrect answer array in QuestionContainer.js

--- src/components/QuestionContainer.js
+++ src/components/QuestionContainer.js
@@ -19,27 +19,43 @@ class QuestionContainer extends Component {
                 return results.json();
             }).then(data => {
                 
-                let questions = data.results.map((question, index) => {
-                    return(
-                        <div key={index} className="questionWrapper">
-                            <div className="question" key={question.question}>
-                                 <RenderHTMLQuestion HTML={question.question} />
-                            </div>
+                // let questions = data.results.map((question, index) => {
+                //     return(
+                //         <div key={index} className="questionWrapper">
+                //             <div className="question" key={question.question}>
+                //                  <RenderHTMLQuestion HTML={question.question} />
+                //             </div>
 
-                            <ul className="answers">
-                                {question.incorrect_answers.map((answer, index) => {
-                                    let correctAnswer = false;
-                                    if(Math.floor(Math.random() * question.incorrect_answers.length-1) === index && !correctAnswer) {
-                                            correctAnswer = true;
-                                            return <> <RenderHTMLAnswer key={question.correct_answer} HTML={"correct " + question.correct_answer} /> <RenderHTMLAnswer key={index} HTML={"incorrect " + answer} /> </>
-                                    } else {
-                                        return <RenderHTMLAnswer key={index} HTML={"incorrect " + answer} />
-                                    }
-                                })}
-                            </ul>
+                //             <ul className="answers">
+                //                 {question.incorrect_answers.map((answer, index) => {
+                //                     let correctAnswer = false;
+                //                     if(Math.floor(Math.random() * question.incorrect_answers.length-1) === index && !correctAnswer) {
+                //                             correctAnswer = true;
+                //                             return <> <RenderHTMLAnswer key={question.correct_answer} HTML={"correct " + question.correct_answer} /> <RenderHTMLAnswer key={index} HTML={"incorrect " + answer} /> </>
+                //                     } else {
+                //                         return <RenderHTMLAnswer key={index} HTML={"incorrect " + answer} />
+                //                     }
+                //                 })}
+                //             </ul>
+                //         </div>
+                //     )
+                // })
+
+                let questions = data.results.map((question, index) => {
+                    question.incorrect_answers.splice(Math.floor(Math.random() * Math.floor(question.incorrect_answers.length + 1)), 0, question.correct_answer)
+                    return (
+                      <div key={index} className="questionWrapper">
+                        <div className="question" key={question.question}>
+                            <RenderHTMLQuestion HTML={question.question} />
                         </div>
+                        <ul className="answers">
+                          {question.incorrect_answers.map((answer, index) => { 
+                              return <RenderHTMLAnswer key={index} HTML={answer} />
+                          })}
+                        </ul>
+                      </div>
                     )
-                })
+                  })
 
                 this.setState({questions: questions});
             })
